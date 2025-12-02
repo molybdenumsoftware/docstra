dbe2a2815ff97dcea76d5ad839f56baa88473967
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-12-02 16:20:27 +0700

    chore: extract docs content file

--- dev/modules/docs/default.nix
+++ dev/modules/docs/default.nix
@@ -1,81 +1,26 @@
 {
+  lib,
   config,
   inputs,
   ...
 }:
 {
-  perSystem =
+  options.documentation.module = lib.mkOption {
+    type = lib.types.deferredModule;
+  };
+  config.perSystem =
     { pkgs, ... }:
     let
       workflowPath = ".github/workflows/publish-website.yaml";
 
       documentation = config.lib.evalDocs {
         modules = [
-          (
-            docArgs:
-            let
-              inherit (docArgs.config.htnl.polymorphic.partials)
-                p
-                a
-                h1
-                h2
-                h3
-                ;
-            in
-            {
-              inherit pkgs;
-              htnl = inputs.htnl.lib;
-              metadata = { inherit (config.metadata) title; };
-              pages = {
-                index = {
-                  title = "Index";
-                  content = [
-                    {
-                      htnl = [
-                        (h1 [
-                          config.metadata.title
-                          ", a "
-                          config.metadata.description.ir
-                        ])
-                        (p "Welcome to the ${config.metadata.title} documentation!")
-                        (p [
-                          "${config.metadata.title} is a "
-                          config.metadata.description.ir
-                          "."
-                        ])
-                        (p [
-                          "So this documentation is "
-                          (a { href = "https://en.wikipedia.org/wiki/Eating_your_own_dog_food"; } "dog food!")
-                        ])
-                      ];
-                    }
-                  ];
-                };
-                introduction = {
-                  title = "Introduction";
-                  content = [
-                    {
-                      htnl = [
-                        (h1 [
-                          "Introduction to ${config.metadata.title}, the "
-                          config.metadata.description.ir
-                        ])
-                        (h2 { id = "background"; } "Background")
-                        (p "Some background")
-                        (h3 { id = "deeper"; } "Deeper")
-                        (p "more")
-                        (h2 { id = "description"; } "Description")
-                        (p config.metadata.description.ir)
-                        (h2 { id = "goals"; } "Goals")
-                      ];
-                    }
-                  ];
-                };
-              };
-            }
-          )
+          {
+            inherit pkgs;
+            htnl = inputs.htnl.lib;
+          }
+          config.documentation.module
         ];
-
       };
 
     in
