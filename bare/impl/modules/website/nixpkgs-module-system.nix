0ecf89e0a6d05f4e8f69902ad9df1c5f42afc869
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-12-03 22:19:01 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:bare/impl/modules/website/nixpkgs-module-system.nix

--- bare/impl/modules/website/nixpkgs-module-system.nix
+++ bare/impl/modules/website/nixpkgs-module-system.nix
@@ -1,6 +1,15 @@
 { lib, config, ... }:
 let
-  inherit (config.htnl.polymorphic.partials) p code div;
+  inherit (config.htnl.polymorphic.partials)
+    p
+    a
+    dl
+    dt
+    dd
+    pre
+    code
+    div
+    ;
 in
 {
   options.subjects.nixpkgsModuleSystem.htnlMappers = lib.mkOption {
@@ -31,29 +40,61 @@ in
     toHtnl =
       v:
       div [
-        (p "Nixpkgs module system configuration")
-        (p [
+        (div { class = "text-xl mt-[1lh]"; } "Nixpkgs module system configuration")
+        (div { class = "mt-[1lh]"; } [
           "Class: "
           (code v.result.class)
         ])
-        (p "Options:")
+        (div { class = "mt-[1lh]"; } "Options:")
         # TODO what about the contents of `_module`?
         (
           v.result.options
           |> lib.optionAttrSetToDocList
           |> map (
             option:
-            lib.trace (lib.attrNames option) (div [
-              (p (code option.name))
+            # TODO `.loc` gives us the individual path parts, so nicer styling possible and perhaps some navigation features
+            lib.trace (lib.attrNames option) (dl [
               (
-                [
-                  { name = "internal"; }
-                  { name = "readOnly"; }
-                ]
-                |> map (
-                  attribute: lib.optional (lib.attrByPath [ attribute.name ] false option) (p (code attribute.name))
-                )
+                let
+                  id = option.name |> lib.replaceStrings [ "<" ">" ] [ "_" "_" ] |> (n: "opt:${n}");
+                in
+                dt
+                  {
+                    inherit id;
+                    class = "mt-[1lh] text-lg";
+                  }
+                  (
+                    a {
+                      href = "#${id}";
+                      class = "border-b";
+                    } (code option.name)
+                  )
               )
+              (dd { class = "py-[1lh] ps-[1ch] border-s"; } [
+                (div { class = "flex gap-2"; } (
+                  [
+                    "internal"
+                    "readOnly"
+                  ]
+                  |> map (
+                    attribute:
+                    lib.optional (lib.attrByPath [ attribute ] false option) (
+                      code { class = "text-sm bg-neutral-700 px-[2px] rounded-sm"; } attribute
+                    )
+                  )
+                ))
+                (lib.optional (lib.isString option.description) (
+                  pre { class = "mt-[1lh]"; } (code option.description)
+                ))
+                # TODO use shared gap instead of these `mt-[1lh]`
+                (div { class = "mt-[1lh]"; } [
+                  "Type: "
+                  option.type
+                ])
+                # TODO declarations
+                # TODO filter by `.visible`
+                # TODO default
+              ])
             ])
           )
         )
