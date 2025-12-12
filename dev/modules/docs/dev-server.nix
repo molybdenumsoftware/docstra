00396be83fb479fe1a8d5e723a6b4c9a3b0c9262
turbio <git@turb.io>
2025-12-11 21:36:37 -0600

    chore: dev-server

--- dev/modules/docs/dev-server.nix
+++ dev/modules/docs/dev-server.nix
@@ -1,7 +1,26 @@
+{ lib, ... }:
 {
   config.perSystem =
-    { pkgs, ... }:
+    { pkgs, system, ... }:
+    let
+      servePath = "serve";
+      buildCommand = ''
+        nix-build $"($git_root)/dev" -A packages.${system}.website --out-link $"($serve_path)/result"
+      '';
+    in
     {
-      make-shells.default.packages = [ pkgs.nodePackages_latest.live-server ];
+      gitignore = [ "/${servePath}" ];
+
+      make-shells.default.packages = [
+        (pkgs.writers.writeNuBin "dev-server" ''
+          let git_root = git rev-parse --show-toplevel
+          let serve_path = $"($git_root)/${servePath}"
+
+          ${buildCommand}
+
+          let live_server = job spawn { ${lib.getExe pkgs.live-server} --poll $serve_path --open result }
+          ${lib.getExe pkgs.watchexec} --postpone ${buildCommand}
+        '')
+      ];
     };
 }
